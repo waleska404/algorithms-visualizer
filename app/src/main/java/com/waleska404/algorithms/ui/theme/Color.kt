1502a8cbc59f05a493093865babb46e01ab684d7
Paula Boyano Ivars <paulaboyanoivars@gmail.com>
2023-11-14 16:57:17 +0100

    :lipstick: contrast topbar icons

--- app/src/main/java/com/waleska404/algorithms/ui/theme/Color.kt
+++ app/src/main/java/com/waleska404/algorithms/ui/theme/Color.kt
@@ -7,6 +7,7 @@ val MainColorDark = Color(0xFF171F2B)
 val LightContrastColorDark = Color(0xFF232D3C)
 val ContrastColorDark = Color(0xFFC2CCE4)
 val SecondaryContrastColorDark = Color(0xFF92D5DB)
+// TODO: quitar la transparencia con color picker
 val AccentContentDark = Color(0xC4999BFF)
 val DisabledColorDark = Color(0xFF3C4C58)
 
@@ -18,7 +19,6 @@ val SecondaryContrastColorLight = Color(0xFF1D292B)
 val AccentContentLight = Color(0xB94649FC)
 val DisabledColorLight = Color(0xFF8A93A7)
 
-
-// sharedr
+// shared
 val AccentGreen = Color(0xFF00FF00)
 
--- app/src/main/java/com/waleska404/algorithms/ui/theme/Theme.kt
+++ app/src/main/java/com/waleska404/algorithms/ui/theme/Theme.kt
@@ -56,7 +56,7 @@ fun AlgorithmsTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
